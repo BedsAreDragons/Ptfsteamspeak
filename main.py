8b8bf5bba5bc654b48f13a0470bee7523d5b2585
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-08-22 07:46:05 -0400

    Update

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('audio')
+def handle_audio(data):
+    # Broadcast the audio data to all other clients
+    emit('audio', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
